f3c418d396b69e90bdacb4fe67752393f6108f15
Shahd Farghaly <shahdfarghaly2005@gmail.com>
2026-07-23 16:25:16 +0300

    Add files via upload

---
+++ Unconstrained_React/shipment_data.py
@@ -0,0 +1,84 @@
+test_cases = [
+# Case 1
+{
+    "shipment_id": "SH001",
+    "delay_reason": "Traffic",
+    "delay_hours": 2,
+    "customer": "Standard",
+    "package": "Electronics",
+    "customer_available": True
+},
+# Case 2
+{
+    "shipment_id": "SH002",
+    "delay_reason": "Vehicle Breakdown",
+    "delay_hours": 1,
+    "customer": "Standard",
+    "package": "Clothes",
+    "customer_available": True
+},
+# Case 3
+{
+    "shipment_id": "SH003",
+    "delay_reason": "Customer Not Available",
+    "delay_hours": 1,
+    "customer": "Standard",
+    "package": "Books",
+    "customer_available": False
+},
+# Case 4
+{
+    "shipment_id": "SH004",
+    "delay_reason": "None",
+    "delay_hours": 0,
+    "customer": "Standard",
+    "package": "Food",
+    "customer_available": True
+},
+# Case 5
+{
+    "shipment_id": "SH005",
+    "delay_reason": "Weather",
+    "delay_hours": 2,
+    "customer": "Standard",
+    "package": "Food",
+    "customer_available": True
+},
+# Case 6
+{
+    "shipment_id": "SH006",
+    "delay_reason": "Traffic",
+    "delay_hours": 5,
+    "customer": "VIP",
+    "package": "Medicine",
+    "customer_available": True
+},
+# Case 7
+{
+    "shipment_id": "SH007",
+    "delay_reason": "Vehicle Breakdown",
+    "delay_hours": 3,
+    "customer": "VIP",
+    "package": "Medicine",
+    "customer_available": True
+},
+# Case 8
+{
+    "shipment_id": "SH008",
+    "delay_reason": "Weather",
+    "delay_hours": 5,
+    "customer": "VIP",
+    "package": "Medicine",
+    "customer_available": True
+},
+# Case 9
+{
+    "shipment_id": "SH009",
+    "delay_reason": "Traffic",
+    "delay_hours": 4,
+    "customer": "Standard",
+    "package": "Documents",
+    "customer_available": False
+}
+
+]
